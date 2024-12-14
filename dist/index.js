c2fd58ef0c6f787b283b6f3e4c5fa17207c24d11
JackTheCoconut <cknganab@connect.ust.hk>
2024-12-14 17:32:48 +0800

    Remove dist and .idea from the repository

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const greeting = document.getElementById("greeting");
-const button = document.getElementById("btn");
-if (greeting && button) {
-    button.addEventListener("click", () => {
-        greeting.textContent = "Hello, GitHub Pages with TypeScript!";
-    });
-}
