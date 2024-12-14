aa4f42c60f4517f11c7afd741e8b4e26333801aa
JackTheCoconut <cknganab@connect.ust.hk>
2024-12-14 17:30:03 +0800

    ts init

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const greeting = document.getElementById("greeting");
+const button = document.getElementById("btn");
+if (greeting && button) {
+    button.addEventListener("click", () => {
+        greeting.textContent = "Hello, GitHub Pages with TypeScript!";
+    });
+}
